79285c68d3dcb2e8ef7df849d4ddaabd367a49c5
nehal.jain <nehal.jain@atos.net>
2016-12-19 17:05:47 +0530

    v1

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'in-cdbr-azure-central-b.cloudapp.net');
+   define('DB_USERNAME', 'bf93a845126411');
+     define('DB_PASSWORD', '7749a8d4');
+   define('DB_DATABASE','acsm_2d98dd3d6443bb1');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) ;
+?>
